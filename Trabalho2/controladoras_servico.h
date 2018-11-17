af35d69adce1a1604edebe040723662108064432
Andre Macedo <andremacedo.pv@gmail.com>
2018-11-17 15:35:17 -0200

    Divisao das controladoras

---
+++ Trabalho2/controladoras_servico.h
@@ -0,0 +1,39 @@
+#ifndef CONTROLADORAS_SERVICO_H_
+#define CONTROLADORAS_SERVICO_H_
+
+#include "dominios_hotel.h"
+#include "dominios_usuario.h"
+#include "entidades.h"
+#include "interfaces.h"
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+class CntrServUsuario:public IServUsuario{
+
+private:
+    std::list<Usuario*> ListaUsuario;
+
+public:
+    Usuario* BuscarUsuario(Identificador identificador, Senha senha);
+    void AdicionarUsuario(Usuario &usuario);
+    void RemoverUsuario(Usuario &usuario) throw (invalid_argument);
+
+
+};
+
+class CntrServAcomodacao:public IServAcomodacao{
+
+private:
+    std::list<Acomodacao*> ListaAcomodacao;
+
+public:
+    Acomodacao* BuscarAcomodacao(Identificador identificador);
+    void AdicionarAcomodacao(Acomodacao &acomodacao);
+    void RemoverAcomodacao(Acomodacao &acomodacao) throw (invalid_argument);
+
+
+};
+
+#endif
